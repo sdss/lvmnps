e5c08cef62f0a58805fe90d3c3853dfebc650070
Florian Briegel <wasndas@zone42.de>
2021-06-29 23:08:33 +0200

    First working test with actor

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,53 @@ underlying directories. See https://docs.pytest.org/en/2.7.3/plugins.html for
 more information.
 """
 
+import asyncio
+import os
+import pathlib
+import shutil
+
+import pytest
+
+from clu import AMQPActor, AMQPClient, JSONActor
+from clu.protocol import open_connection
+
+
+import shutil
+from pytest_rabbitmq import factories
+
+rabbitmq_local_proc=shutil.which('rabbitmq-server', path="/usr/local/sbin:/usr/sbin")
+rabbitmq_proc = factories.rabbitmq_proc(host='127.0.0.1',
+                                        port=None, 
+                                        node="test",
+                                        logsdir='/tmp/rabbitmq/logs', 
+                                        plugindir='/usr/lib64/rabbitmq/lib/rabbitmq_server-3.8.11/plugins/', 
+                                        server=rabbitmq_local_proc,
+                                        ctl=f"{os.path.dirname(rabbitmq_local_proc)}/rabbitmqctl")
+
+
+   
+@pytest.fixture
+async def amqp_actor(rabbitmq, event_loop):
+
+    port = rabbitmq.args["port"]
+
+    actor = AMQPActor(name="amqp_actor", port=port)
+    await actor.start()
+
+    yield actor
+
+    await actor.stop()
+
+
+@pytest.fixture
+async def amqp_client(rabbitmq, amqp_actor, event_loop):
+
+    port = rabbitmq.args["port"]
+
+    client = AMQPClient(name="amqp_client", models=["amqp_actor"], port=port)
+    await client.start()
+
+    yield client
+
+    await client.stop()
+
