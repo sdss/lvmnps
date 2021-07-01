eea75c79617a19ede0a69e04818521ba60875918
Florian Briegel <wasndas@zone42.de>
2021-07-01 10:55:12 +0200

    added test with pytest-rabbitmq

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,25 +10,25 @@ underlying directories. See https://docs.pytest.org/en/2.7.3/plugins.html for
 more information.
 """
 
-# import os
-# import shutil
+import os
+import shutil
 
 import pytest
 from clu import AMQPActor, AMQPClient
 
 
-# from pytest_rabbitmq import factories
+from pytest_rabbitmq import factories
 
 
-# rabbitmq_local_proc = shutil.which('rabbitmq-server', path="/usr/local/sbin:/usr/sbin")
-# rabbitmq_local_plugindir = '/usr/lib64/rabbitmq/lib/rabbitmq_server-3.8.11/plugins/'
-# rabbitmq_proc = factories.rabbitmq_proc(host='127.0.0.1',
-#                                         port=None,
-#                                         node="test",
-#                                         logsdir='/tmp/rabbitmq/logs',
-#                                         plugindir=rabbitmq_local_plugindir,
-#                                         server=rabbitmq_local_proc,
-#                                         ctl=f"{os.path.dirname(rabbitmq_local_proc)}/rabbitmqctl")
+rabbitmq_local_proc = shutil.which('rabbitmq-server', path="/usr/local/sbin:/usr/sbin")
+rabbitmq_local_plugindir = '/usr/lib64/rabbitmq/lib/rabbitmq_server-3.8.11/plugins/'
+rabbitmq_proc = factories.rabbitmq_proc(host='127.0.0.1',
+                                        port=None,
+                                        node="test",
+                                        logsdir='/tmp/rabbitmq/logs',
+                                        plugindir=rabbitmq_local_plugindir,
+                                        server=rabbitmq_local_proc,
+                                        ctl=f"{os.path.dirname(rabbitmq_local_proc)}/rabbitmqctl")
 
 
 @pytest.fixture
