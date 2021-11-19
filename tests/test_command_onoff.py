d0bffd86f89e9d749c61ed4c8fb30686892cb5ce
changgonKim <changgonkim@khu.ac.kr>
2021-11-18 22:51:53 -0800

    coverage test

--- tests/test_command_onoff.py
+++ tests/test_command_onoff.py
@@ -6,51 +6,12 @@ import pytest
 
 pytestmark = [pytest.mark.asyncio]
 
-"""
-async def test_on(actor: NpsActor):
-    command = await actor.invoke_mock_command("status nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-    print(command.replies[2].message)
+@pytest.fixture(scope='session')
+async def test_actor():
 
-    # assert len(command.replies) == 1
+    _actor = setup_test_actor(LegacyActor('my_actor', host='localhost', port=9999))
 
-    command = await actor.invoke_mock_command("on nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
+    yield _actor
 
-    command = await actor.invoke_mock_command("status nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-    print(command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"])
-    assert command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"] == 1
-
-
-async def test_off(actor: NpsActor):
-    command = await actor.invoke_mock_command("status nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-    print(command.replies[2].message)
-
-    # assert len(command.replies) == 1
-
-    command = await actor.invoke_mock_command("on nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-
-    command = await actor.invoke_mock_command("status nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-    print(command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"])
-    assert command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"] == 1
-
-    command = await actor.invoke_mock_command("off nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-
-    command = await actor.invoke_mock_command("status nps_dummy_1 1")
-    await command
-    assert command.status.did_succeed
-    print(command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"])
-    assert command.replies[2].message["status"]["nps_dummy_1"]["port1"]["state"] == 0
-"""
+    # Clear replies
+    _actor.mock_replies.clear()
