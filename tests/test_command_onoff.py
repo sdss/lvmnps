9ee0fc326a7fee0f7e30589ba7c5fdddb4ba00d3
changgonKim <changgonkim@khu.ac.kr>
2021-12-02 21:24:15 -0800

    update for rabbitmq unit testing

--- tests/test_command_onoff.py
+++ tests/test_command_onoff.py
@@ -5,13 +5,3 @@ import pytest
 
 
 pytestmark = [pytest.mark.asyncio]
-
-@pytest.fixture(scope='session')
-async def test_actor():
-
-    _actor = setup_test_actor(LegacyActor('my_actor', host='localhost', port=9999))
-
-    yield _actor
-
-    # Clear replies
-    _actor.mock_replies.clear()
