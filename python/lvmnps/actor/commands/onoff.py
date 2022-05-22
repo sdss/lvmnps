1f015df1e6c3305241270eebaf0ac06a31e19262
José Sánchez-Gallego <gallegoj@uw.edu>
2022-05-22 15:24:27 -0700

    Tweak on/off commands

--- python/lvmnps/actor/commands/onoff.py
+++ python/lvmnps/actor/commands/onoff.py
@@ -26,7 +26,7 @@ async def switch_control(
     switch: PowerSwitchBase,
     on: bool,
     name: str,
-    portnum: int,
+    portnum: int | None,
 ):
     """The function for parsing the actor command to the switch library."""
 
@@ -43,91 +43,112 @@ async def switch_control(
 
 
 @parser.command()
-@click.argument("NAME", type=str, required=False)
-@click.argument("PORTNUM", type=int, required=False, default=0)
-@click.argument("OFFAFTER", type=int, default=0)
+@click.argument("OUTLET", type=str)
+@click.argument("PORTNUM", type=int, required=False)
+@click.option(
+    "--switch",
+    type=str,
+    help="Address this switch specifically. Otherwise the first switch "
+    "with an outlet that matches NAME will be commanded.",
+)
+@click.option("--off-after", type=float, help="Turn off after X seconds.")
 async def on(
     command: NPSCommand,
     switches: dict[str, PowerSwitchBase],
-    name: str,
-    portnum: int,
-    offafter: int,
+    outlet: str,
+    portnum: int | None = None,
+    switch: str | None = None,
+    off_after: float | None = None,
 ):
     """Turn on the outlet."""
 
     if portnum:
-        command.info(text=f"Turning on {name} port {portnum}...")
+        command.info(text=f"Turning on {outlet} port {portnum}...")
     else:
-        command.info(text=f"Turning on Outlet {name}...")
-
-    # TODO: this could fail if multiple switches have outlets with the same name.
+        command.info(text=f"Turning on outlet {outlet}...")
 
     the_switch: PowerSwitchBase | None = None
     current_status: dict | None = None
-    for switch in switches.values():
-        current_status = await switch.statusAsDict(name, portnum)
+    for sw in switches.values():
+        if switch and sw.name != switch:
+            continue
+
+        current_status = await sw.statusAsDict(outlet, portnum)
         if current_status:
-            the_switch = switch
+            the_switch = sw
             break
 
     if current_status is None or the_switch is None:
-        return command.fail(f"Could not find a match for {name}:{portnum}.")
+        return command.fail(f"Could not find a match for {outlet}:{portnum}.")
 
     outletname_list = list(current_status.keys())
     outletname = outletname_list[0]
 
     if current_status[outletname]["state"] == 0:
-        current_status = await switch_control("on", the_switch, True, name, portnum)
+        current_status = await switch_control("on", the_switch, True, outlet, portnum)
     elif current_status[outletname]["state"] == 1:
-        return command.fail(text=f"The Outlet {outletname} is already ON")
+        return command.finish(text=f"The outlet {outletname} is already ON")
     else:
-        return command.fail(text=f"The Outlet {outletname} returns wrong value")
+        return command.fail(text=f"The outlet {outletname} returns wrong value")
 
     command.info(status=current_status)
-    if offafter > 0:
-        command.info(f"The switch will be turned off after {offafter} seconds.")
-        await asyncio.sleep(offafter - 1)
-        current_status = await switch_control("off", the_switch, False, name, portnum)
+
+    if off_after is not None:
+        command.info(f"The switch will be turned off after {off_after} seconds.")
+        await asyncio.sleep(off_after - 1)
+        current_status = await switch_control("off", the_switch, False, outlet, portnum)
         command.info(status=current_status)
+
     return command.finish()
 
 
 @parser.command()
-@click.argument("NAME", type=str, default="")
-@click.argument("PORTNUM", type=int, default=0)
+@click.argument("OUTLET", type=str)
+@click.argument("PORTNUM", type=int, required=False)
+@click.option(
+    "--switch",
+    type=str,
+    help="Address this switch specifically. Otherwise the first switch "
+    "with an outlet that matches NAME will be commanded.",
+)
 async def off(
     command: NPSCommand,
     switches: dict[str, PowerSwitchBase],
-    name: str,
-    portnum: int,
+    outlet: str,
+    portnum: int | None = None,
+    switch: str | None = None,
 ):
     """Turn off the outlet."""
 
     if portnum:
-        command.info(text=f"Turning off {name} port {portnum}...")
+        command.info(text=f"Turning off {outlet} port {portnum}...")
     else:
-        command.info(text=f"Turning off Outlet {name}...")
+        command.info(text=f"Turning off outlet {outlet}...")
 
     the_switch: PowerSwitchBase | None = None
     current_status: dict | None = None
-    for switch in switches.values():
-        current_status = await switch.statusAsDict(name, portnum)
+    for sw in switches.values():
+        if switch and sw.name != switch:
+            continue
+
+        current_status = await sw.statusAsDict(outlet, portnum)
         if current_status:
-            the_switch = switch
+            the_switch = sw
             break
 
     if current_status is None or the_switch is None:
-        return command.fail(f"Could not find a match for {name}:{portnum}.")
+        return command.fail(f"Could not find a match for {outlet}:{portnum}.")
 
     outletname_list = list(current_status.keys())
     outletname = outletname_list[0]
 
     if current_status[outletname]["state"] == 1:
-        current_status = await switch_control("off", the_switch, False, name, portnum)
+        current_status = await switch_control("off", the_switch, False, outlet, portnum)
     elif current_status[outletname]["state"] == 0:
-        return command.fail(text=f"The Outlet {outletname} is already OFF")
+        return command.finish(text=f"The outlet {outletname} is already OFF")
     else:
-        return command.fail(text=f"The Outlet {outletname} returns wrong value")
+        return command.fail(text=f"The outlet {outletname} returns wrong value")
 
     command.info(status=current_status)
+
     return command.finish()
