7272f1b0a3fdeefa747f23bdd2762ee1b971843d
José Sánchez-Gallego <gallegoj@uw.edu>
2023-11-22 18:33:50 -0800

    Add placeholder test

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-11-22
+# @Filename: conftest.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
---
+++ tests/test_nps.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-11-22
+# @Filename: test_nps.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+
+async def test_nps_actor():
+    assert True
